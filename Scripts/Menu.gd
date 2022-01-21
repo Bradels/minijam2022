166574c4907032075791dcc35487fde5dbe7698e
Hayden Whitehead <hayden.whitehead@protonmail.com>
2022-01-21 23:03:00 +1100

    Server code

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -2,15 +2,26 @@ extends Control
 
 
 func _ready():
-	pass
-
+	Server.connect("player_connected", self, "_on_player_connected")
+	Server.connect("player_disconnected", self, "_on_player_disconnected")
+	Server.connect("connection_successful", self, "_on_connection_successful")
+	
+	change_menu("MainMenu")
+	
+func change_menu(menu_name):
+	$MainMenu.visible = false
+	$HostMenu.visible = false
+	$JoinMenu.visible = false
+	$LobbyMenu.visible = false
+	get_node(menu_name).visible = true
+	
  # Main Menu functions
 func _on_MainMenu_HostButton_pressed():
-	pass # Replace with function body.
+	change_menu("HostMenu")
 
 
 func _on_MainMenu_JoinButton_pressed():
-	pass # Replace with function body.
+	change_menu("JoinMenu")
 
 
 func _on_MainMenu_OptionsButton_pressed():
@@ -18,4 +29,61 @@ func _on_MainMenu_OptionsButton_pressed():
 
 
 func _on_MainMenu_ExitButton_pressed():
+	get_tree().quit()
+
+
+# Host Menu functions
+func _on_HostMenu_HostButton_pressed():
+	var player_name = $HostMenu/Panel/PlayerName.text
+	var port = int($HostMenu/Panel/Port.text)
+	var max_players = int($HostMenu/Panel/MaxPlayers.text)
+	Server.start_server(port, max_players)
+	Server.set_player_static_data({"n": player_name})
+	update_lobby_player_list()
+	change_menu("LobbyMenu")
+
+
+func _on_HostMenu_BackButton_pressed():
+	change_menu("MainMenu")
+
+
+# Join Menu functions
+func _on_JoinMenu_JoinButton_pressed():
+	var player_name = $JoinMenu/Panel/PlayerName.text
+	var ip = $JoinMenu/Panel/Ip.text
+	var port = int($JoinMenu/Panel/Port.text)
+	Server.join_server(ip, port)
+	change_menu("LobbyMenu")
+
+
+func _on_JoinMenu_BackButton_pressed():
+	change_menu("MainMenu")
+
+
+# Lobby Menu functions
+func _on_LobbyMenu_ReadyButton_pressed():
 	pass # Replace with function body.
+
+
+func _on_LobbyMenu_LeaveButton_pressed():
+	Server.close_server()
+	change_menu("MainMenu")
+
+func update_lobby_player_list():
+	$LobbyMenu/Panel/PlayerList.clear()
+	for player in Server.player_static_data.values():
+		$LobbyMenu/Panel/PlayerList.add_item(player["n"])
+
+
+func _on_player_connected():
+	update_lobby_player_list()
+	
+	
+func _on_player_disconnected():
+	update_lobby_player_list()
+	
+
+func _on_connection_successful():
+	var player_name = $JoinMenu/Panel/PlayerName.text
+	Server.set_player_static_data({"n": player_name})
+	change_menu("LobbyMenu")
--- Scripts/Server.gd
+++ Scripts/Server.gd
@@ -1,5 +1,97 @@
 extends Node
 
+signal player_connected
+signal player_disconnected
+signal connection_successful
+
+var network
+var hosting = false
+
+# World data
+var player_static_data = {}
+var world = {}
 
 func _ready():
 	pass
+
+
+func start_server(port, max_players):
+	hosting = true
+	network = NetworkedMultiplayerENet.new()
+	network.create_server(port, max_players)
+	get_tree().set_network_peer(network)
+	
+	network.connect("peer_connected", self, "_peer_connected")
+	network.connect("peer_disconnected", self, "_peer_disconnected")
+	
+
+func close_server():
+	if hosting:
+		rpc("player_disconnecting")
+	else:
+		rpc_id(1, "player_disconnecting")
+	get_tree().set_network_peer(null)
+	
+
+func join_server(ip, port):
+	hosting = false
+	network = NetworkedMultiplayerENet.new()
+	network.create_client(ip, port)
+	get_tree().set_network_peer(network)
+	
+	network.connect("connection_failed", self, "_connection_failed")
+	network.connect("connection_succeeded", self, "_connection_succeeded")
+
+
+func _peer_connected(player_id):
+	print("Peer connected: " + str(player_id))
+
+
+func _peer_disconnected(player_id):
+	print("Peer disconnected: " + str(player_id))
+	
+	
+func _connection_failed():
+	print("Connection failed")
+	
+
+func _connection_succeeded():
+	print("Connection succeeded")
+	emit_signal("connection_successful")
+	
+
+remote func player_disconnecting():
+	var player_id = get_tree().get_rpc_sender_id()
+	if hosting:
+		player_static_data.erase(player_id)
+		rpc("host_update_player_static_data", player_static_data)
+	else:
+		close_server()
+	emit_signal("player_disconnected")
+		
+	
+func set_player_static_data(data):
+	if hosting:
+		player_static_data[1] = data
+		rpc("host_update_player_static_data", player_static_data)
+	else:
+		rpc_id(1, "client_update_player_static_data", data)
+		
+remote func host_update_player_static_data(data):
+	var player_id = get_tree().get_network_unique_id()
+	if player_static_data.has(player_id):
+		var my_data = player_static_data[player_id]
+		player_static_data = data
+		player_static_data[player_id] = my_data
+	else:
+		player_static_data = data
+	
+	print("emmitting signal")
+	emit_signal("player_connected")
+	
+remote func client_update_player_static_data(data):
+	var player_id = get_tree().get_rpc_sender_id()
+	player_static_data[player_id] = data
+	rpc("host_update_player_static_data", player_static_data)
+	print("emmitting signal")
+	emit_signal("player_connected")
