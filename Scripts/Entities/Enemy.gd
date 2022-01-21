be2c8faa2a5d250c7796b24436ae1f48e32c08a7
arrisar <james@arrisar.com.au>
2022-01-21 23:43:27 +1100

    feat: spaceship entity and sprites with exhaust animation

--- Scripts/Entities/Enemy.gd
+++ Scripts/Entities/Enemy.gd
@@ -3,5 +3,11 @@ extends KinematicBody2D
 onready var _animation_player = $AnimationPlayer
 
 func _physics_process(_delta):
-	var player_pos = get_parent().get_node("Player").global_position
-	look_at(player_pos)
+	var player = get_parent().get_node_or_null("Player")
+	if (player):
+		look_at(player.global_position)
+
+	var spaceship = get_parent().get_node_or_null("Spaceship")
+	if (spaceship):
+		look_at(spaceship.global_position)
+
