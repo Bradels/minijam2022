0c36b0b7d8bafffe6566e6f4cfddb7b20727559d
Hayden Whitehead <hayden.whitehead@protonmail.com>
2022-01-21 18:53:55 +1100

    Menu and server

---
+++ scripts/Menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+func _ready():
+	pass
+
+ # Main Menu functions
+func _on_MainMenu_HostButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_MainMenu_JoinButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_MainMenu_OptionsButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_MainMenu_ExitButton_pressed():
+	pass # Replace with function body.
