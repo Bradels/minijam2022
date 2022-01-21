40fe8dd65a0270efa41642760aef0cbc1f845f11
arrisar <james@arrisar.com.au>
2022-01-21 18:44:27 +1100

    feat: add player control, begin bullet, capital folders singular

--- Player.gd
+++
@@ -1,9 +0,0 @@
-extends KinematicBody2D
-
-var baseMoveSpeed = 500
-
-func _ready():
-	pass
-
-func _physics_process(delta):
-	var motion = Vector2()
---
+++ Script/Player.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+var base_move_speed = 256
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	var motion = Vector2()
+
+	if Input.is_action_pressed('up'):
+		motion.y -= 1
+
+	if Input.is_action_pressed('down'):
+		motion.y += 1
+		
+	if Input.is_action_pressed('left'):
+		motion.x -= 1
+		
+	if Input.is_action_pressed('right'):
+		motion.x += 1
+	
+	motion = motion.normalized()
+	motion = move_and_slide(motion * base_move_speed)
+	
+	look_at(get_global_mouse_position())
+	
