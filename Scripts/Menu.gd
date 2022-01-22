67caa0fdf52f222a5a7c98beee9a6ea66efaa84f
Hayden Whitehead <hayden.whitehead@protonmail.com>
2022-01-22 21:16:55 +1100

    Added puase menu

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -2,9 +2,9 @@ extends Control
 
 var title_scale_speed = 0.0001
 var title_scale_dir = 1
+var is_in_game = false
 
 func _ready():
-	($ButtonHover.stream as AudioStreamMP3).loop = false
 	Server.connect("player_connected", self, "_on_player_connected")
 	Server.connect("player_disconnected", self, "_on_player_disconnected")
 	Server.connect("connection_successful", self, "_on_connection_successful")
@@ -13,21 +13,31 @@ func _ready():
 	
 
 func _process(delta):
-	$Planet.rect_position.x += 0.05
-	$Title.rect_scale += Vector2(title_scale_speed * title_scale_dir, title_scale_speed * title_scale_dir)
-	if $Title.rect_scale.x > 1.2:
+	$Background/Planet.rect_position.x += 0.06
+	$Background/Planet.rect_position.y += 0.04
+	$Background/Title.rect_scale += Vector2(title_scale_speed * title_scale_dir, title_scale_speed * title_scale_dir)
+	if $Background/Title.rect_scale.x > 1.2:
 		title_scale_dir = -1
-		$Title.rect_scale = Vector2(1.2, 1.2)
+		$Background/Title.rect_scale = Vector2(1.2, 1.2)
 	
-	if $Title.rect_scale.x < 1.0:
+	if $Background/Title.rect_scale.x < 1.0:
 		title_scale_dir = 1
-		$Title.rect_scale = Vector2(1.0, 1.0)
+		$Background/Title.rect_scale = Vector2(1.0, 1.0)
+		
+	if Input.is_action_just_pressed("ui_cancel"):
+		if is_in_game:
+			$PauseMenu.visible = !$PauseMenu.visible
 	
 func change_menu(menu_name):
+	$Background.visible = true
 	$MainMenu.visible = false
 	$HostMenu.visible = false
 	$JoinMenu.visible = false
 	$LobbyMenu.visible = false
+	$PauseMenu.visible = false
+	if menu_name == "":
+		$Background.visible = false
+		return
 	get_node(menu_name).visible = true
 	
  # Main Menu functions
@@ -79,8 +89,9 @@ func _on_JoinMenu_BackButton_pressed():
 func _on_LobbyMenu_ReadyButton_pressed():
 	var scene = load("res://Scenes/Locations/Location.tscn")
 	get_node("/root/Main").add_child(scene.instance())
-	visible = false
+	change_menu("")
 	$MenuMusic.stop()
+	is_in_game = true
 
 
 func _on_LobbyMenu_LeaveButton_pressed():
@@ -109,3 +120,17 @@ func _on_connection_successful():
 
 func _on_button_hover():
 	$ButtonHover.play(0.0)
+
+
+func _on_PauseMenu_ResumeButton_pressed():
+	$PauseMenu.visible = false
+
+
+func _on_PauseMenu_MenuButton_pressed():
+	get_node("/root/Main/Location").queue_free()
+	is_in_game = false
+	change_menu("MainMenu")
+
+
+func _on_PauseMenu_ExitButton_pressed():
+	get_tree().quit()
