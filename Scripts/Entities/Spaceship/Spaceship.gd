ecd2ce9ec60e007aaf85a5410fdb5fe3cae2cbc9
arrisar <james@arrisar.com.au>
2022-01-22 19:23:49 +1100

    tweak: max velocity on spaceship

--- Scripts/Entities/Spaceship/Spaceship.gd
+++ Scripts/Entities/Spaceship/Spaceship.gd
@@ -8,8 +8,9 @@ onready var hud = $Camera/Canvas/HUD
 
 var acceleration : Vector2 = Vector2.ZERO
 var velocity : Vector2 = Vector2.ZERO
-var max_velocity : int = 256
-var thrust : int = 256
+var max_velocity : int = 1024
+var primary_thrust : int = 256
+var secondary_thrust : int = 128
 
 
 func _physics_process(delta):
@@ -17,31 +18,32 @@ func _physics_process(delta):
 	acceleration = Vector2.ZERO
 	
 	if Input.is_action_pressed('ship_rotate_left'):
-		rotation -= thrust * delta * 0.01
+		rotation -= secondary_thrust * delta * 0.02
 		thrusters.rotate_left()
 		
 	if Input.is_action_pressed('ship_rotate_right'):
-		rotation += thrust * delta * 0.01
+		rotation += secondary_thrust * delta * 0.02
 		thrusters.rotate_right()
 
 	if Input.is_action_pressed('ship_forward'):
-		acceleration.x += thrust
+		acceleration.x += primary_thrust
 		thrusters.forward()
 
 	if Input.is_action_pressed('ship_reverse'):
-		acceleration.x -= thrust
+		acceleration.x -= secondary_thrust
 		thrusters.reverse()
 		
 	if Input.is_action_pressed('ship_strafe_right'):
-		acceleration.y += thrust
+		acceleration.y += secondary_thrust
 		thrusters.strafe_right()
 		
 	if Input.is_action_pressed('ship_strafe_left'):
-		acceleration.y -= thrust
+		acceleration.y -= secondary_thrust
 		thrusters.strafe_left()
 	
 	acceleration = acceleration.rotated(rotation)
 	velocity += acceleration * delta
+	velocity = velocity.clamped(max_velocity)
 	position += move_and_slide(velocity * delta)
 	
 	hud.position(position)
