342248bcfef2d7795c28918f72dc83bf1b9d7237
Brad Hill <brad-hill@hotmail.com.au>
2022-01-23 11:53:32 +1100

    Projectile Manager works with pawn

--- Components/Managers/ProjectileManager/ProjectileManager.gd
+++ Components/Managers/ProjectileManager/ProjectileManager.gd
@@ -2,6 +2,8 @@ extends Node
 
 class_name ProjectileManager
 
+onready var level = get_tree().root.find_node("Level",true,false)
+var projectile_node
 
 enum PROJECTILE_TYPE {SHIP_PRIMARY,SHIP_SECONDARY,PAWN_PRIMARY}
 
@@ -13,22 +15,34 @@ var pawn_primary_projectile = preload("res://Components/Entities/Projectiles/Bul
 var projectiles = {}
 
 var projectile_type_map = {
-	"pawn_bullet" : PROJECTILE_TYPE.PAWN_PRIMARY,
+	"PAWN_PRIMARY" : PROJECTILE_TYPE.PAWN_PRIMARY,
 	"ship_prime" : PROJECTILE_TYPE.SHIP_PRIMARY,
 	"ship_secondary": PROJECTILE_TYPE.SHIP_SECONDARY
 }
 
-func _on_projectile_fired(type,props) -> void:
+func _on_projectile_fired(props) -> void:
+	var type = projectile_type_map[props["type"]]
+	var position = props["position"]
+	var rotation = props["rotation"]
+	var velocity = props["velocity"]
+	var ownerid = props["owner_id"]
+	
 	
 	spawn_projectile(
-		projectile_type_map[props["type"]],
-		props["posistion"],
-		props["rotation"],
-		props["velocity"],
-		props["id"],
-		props["ownerid"])
+		type,
+		position,
+		rotation,
+		velocity,
+		str(ownerid))
 		
-	if is_multiplayer: rpc("rpc_spawn_project",type,props)
+	if is_multiplayer: rpc(
+		"spawn_projectile",
+		type,
+		position,
+		rotation,
+		velocity,
+		str(ownerid)
+		)
 
 remote func spawn_projectile(
 	type:int,
@@ -36,11 +50,17 @@ remote func spawn_projectile(
 	rotation:float,
 	velocity:Vector2 = Vector2.ZERO,
 	id:String = "",
-	ownerid:String = "") -> void:
-	var scene = _look_up_projectile(type)
-	var instance = scene.instance()
-	add_child(instance)
-	instance.position = posistion
+	ownerid:String = "") -> String:
+		var scene = _look_up_projectile(type)
+		var instance = scene.instance()
+		level.add_child(instance)
+		instance.position = posistion
+		instance.rotation = rotation
+		instance.velocity = velocity
+		if id == "": id = ownerid+instance.get_instance_id()
+		projectiles[id] = instance
+		return id
+	
 
 func _look_up_projectile(projectile_type:int) -> PackedScene:
 	if projectile_type == PROJECTILE_TYPE.SHIP_PRIMARY:
@@ -50,3 +70,11 @@ func _look_up_projectile(projectile_type:int) -> PackedScene:
 	if projectile_type == PROJECTILE_TYPE.PAWN_PRIMARY:
 		return pawn_primary_projectile
 	return null
+
+func _ready():
+	call_deferred('_setup')
+
+func _setup():
+	projectile_node = Node2D.new()
+	projectile_node.name = 'Projectile'
+	level.add_child(projectile_node)
