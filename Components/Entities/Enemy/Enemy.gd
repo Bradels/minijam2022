a3282cfb019bc59fb4abdfad6acea20cc219a645
arrisar <james@arrisar.com.au>
2022-01-23 16:49:02 +1100

    fix: enemy animations

--- Components/Entities/Enemy/Enemy.gd
+++ Components/Entities/Enemy/Enemy.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 signal died(id)
 
 
-enum ENEMY_STATES {IDLE,FOLLOWING,CLAWING,SHOOTING,DYING}
+enum ENEMY_STATES { IDLE, FOLLOWING, CLAWING, SHOOTING, DYING }
 
 var id = ''
 var velocity = Vector2.ZERO
@@ -12,32 +12,26 @@ var nearest_player
 
 onready var is_host = get_tree().get_network_unique_id() in [0, 1]
 onready var _animation_player = $AnimationPlayer
-onready var _state = ENEMY_STATES.IDLE
 
-export(float) var search_radius = 200
-export(float) var attack_radius = 70
-export(float) var enemy_speed = 45
-export(int) var enemy_health = 2
-export(int) var _enemy_damage = 1
+export(int) var health = 2
+export(int) var damage = 1
+export(float) var speed = 48
+export(float) var search_radius = 256
+export(float) var attack_radius = 64
 
 
 func _process(_delta):
 	set_nearest_player_or_null()
 	
-	if is_host:
-		_determine_state()
-		
-	if _state == ENEMY_STATES.CLAWING:
-		attack()
-
-	if _state == ENEMY_STATES.FOLLOWING:
-		follow_player()
-
-	if _state == ENEMY_STATES.IDLE:
-		idle()
-
-	if _state == ENEMY_STATES.DYING:
-		death()
+	match _determine_state():
+		ENEMY_STATES.CLAWING:
+			attack()
+		ENEMY_STATES.FOLLOWING:
+			follow_player()
+		ENEMY_STATES.IDLE:
+			idle()
+		ENEMY_STATES.DYING:
+			death()
 
 
 func _physics_process(_delta):
@@ -50,9 +44,9 @@ func _set_velocity(new_velocity):
 		velocity = new_velocity
 
 
-func _queue_animation(name):
+func _play_animation(name):
 	if _animation_player.current_animation != name:
-		_animation_player.queue(name)
+		_animation_player.play(name)
 
 
 func _look_at(rotation):
@@ -61,23 +55,22 @@ func _look_at(rotation):
 
 
 func _determine_state():
-	if _state == ENEMY_STATES.DYING || enemy_health <= 0:
-		_state = ENEMY_STATES.DYING
-		return
+	if health <= 0:
+		return ENEMY_STATES.DYING
 	
 	if nearest_player == null:
-		return
+		return ENEMY_STATES.IDLE
 
 	var distance_to_nearest_player = global_position.distance_to(nearest_player.global_position)
 	
 	if distance_to_nearest_player < search_radius:
-		_state = ENEMY_STATES.FOLLOWING
+		return ENEMY_STATES.FOLLOWING
 
 	if distance_to_nearest_player > search_radius:
-		_state = ENEMY_STATES.IDLE
+		return ENEMY_STATES.IDLE
 
 	if distance_to_nearest_player < attack_radius:
-		_state = ENEMY_STATES.CLAWING
+		return ENEMY_STATES.CLAWING
 
 
 func set_nearest_player_or_null():
@@ -100,30 +93,30 @@ func follow_player():
 		return
 	
 	_look_at(nearest_player.global_position)
-	_set_velocity(transform.x * enemy_speed)
-	_queue_animation('Walk')
+	_play_animation('Walk')
+	_set_velocity(transform.x * speed)
 
 
 func idle():
+	_play_animation('Idle')
 	_set_velocity(Vector2.ZERO)
-	_queue_animation('Idle')
 
 
 func hurt(amount):
-	if _state == ENEMY_STATES.DYING:
+	if health <= 0:
 		return
 
-	_animation_player.play("Hurt")
-	enemy_health -= amount
+	_play_animation("Hurt")
+	health -= amount
 
 
 func death():
-	_animation_player.play("Death")
+	_play_animation("Death")
 	_set_velocity(Vector2.ZERO)
 
 
 func attack():
-	_queue_animation('Attack')
+	_play_animation('Attack')
 
 
 func _on_AnimationPlayer_animation_finished(name):
